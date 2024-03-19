a1798bf2cb233227bc0473c432848d0df9338c32
Valeriya Kosykh <valeriya.kosyh2001@gmail.com>
2024-03-19 14:15:00 +0300

    111

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int p,a, b;
+    std::cout << "a=" ;
+    std::cin >> a;
+    std::cout << "b=";
+    std::cin >> b;
+    p = a * b;
+    Привет 
+    std::cout << p;
+}
+
+
